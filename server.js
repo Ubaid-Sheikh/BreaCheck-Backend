ebbe40e51c3c779255856f4019e68a46eba464f2
Ubaid-Sheikh <ubaidsheikh800@gmail.com>
2025-11-01 19:45:47 +0500

    inital commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.post('/check-password', async (req, res) => {
+    try {
+      const { prefix, suffix } = req.body;
+  
+      const response = await fetch(`https://api.pwnedpasswords.com/range/${prefix}`);
+      const text = await response.text();
+      const lines = text.split('\n');
+      const match = lines.find(line => line.startsWith(suffix.toUpperCase()));
+  
+      if (match) {
+        const count = match.split(':')[1].trim();
+        return res.json({ status: 'success', breached: true, count });
+      } else {
+        return res.json({ status: 'success', breached: false });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
